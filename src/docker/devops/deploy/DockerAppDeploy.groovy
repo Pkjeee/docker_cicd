2ea8d0f798c8dbb7debb5741d7e7b399b4eb9927
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-08 20:04:52 +0530

    Resolution for [ssh-agent] Could not find specified credentials

--- src/docker/devops/deploy/DockerAppDeploy.groovy
+++ src/docker/devops/deploy/DockerAppDeploy.groovy
@@ -17,9 +17,9 @@ def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTA
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => UnDeploy Docker Conatiner is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
 	for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-            sshagent(['PASSWORD']) {
+//            sshagent(['PASSWORD']) {
             sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker rm -f ${CONTAINER_NAME}"            
-         }
+//         }
        }
      }
    }
@@ -42,9 +42,9 @@ def DeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String DOCKER_
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => Docker WebApp Deployment is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
         for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-        sshagent(['PASSWORD']) {
+//        sshagent(['PASSWORD']) {
         sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}"
-         }
+//         }
        }
      }
    }
