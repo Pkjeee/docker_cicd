b6d599ffd45dfd35626faf028bc141ae372f2945
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-08 22:58:42 +0530

    added few steps 08-Sep-2018

--- src/docker/devops/deploy/DockerAppDeploy.groovy
+++ src/docker/devops/deploy/DockerAppDeploy.groovy
@@ -12,16 +12,14 @@ package docker.devops.deploy
 ********************************************************************** *****/
 
 def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTAINER_NAME) 
-
 {
    try {
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => UnDeploy Docker Conatiner is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
 	for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
         def DockerRm = "docker rm -f ${CONTAINER_NAME}"
-        def DockerRun = "docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}"
  	     sshagent(['SSH-KEY-102']) {
-		sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} ${DockerRm} ${DockerRun}"
+		sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} ${DockerRm}"
 	 }
        }
      }
@@ -40,7 +38,6 @@ def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTA
 ****************************************************************************/
 
 def ReDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String DOCKER_USER, String DOCKER_APP_NAME, String DOCKER_TAG, String CONTAINER_NAME)
-
 {
    try {
       wrap([$class: 'AnsiColorBuildWrapper']) {
