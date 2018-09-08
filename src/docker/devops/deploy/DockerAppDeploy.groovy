bd1ea0b1fa1efa723ac3217cfb8c938d41250b38
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-08 20:01:08 +0530

    Resolution for [ssh-agent] Could not find specified credentials

--- src/docker/devops/deploy/DockerAppDeploy.groovy
+++ src/docker/devops/deploy/DockerAppDeploy.groovy
@@ -11,14 +11,14 @@ package docker.devops.deploy
 ******	Function to stop & undeploye earlir  Containers 	     *******
 ********************************************************************** *****/
 
-def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTAINER_NAME, String LINUX_CREDENTIALS) 
+def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTAINER_NAME) 
 {
    try {
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => UnDeploy Docker Conatiner is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
 	for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-            sshagent(["${LINUX_CREDENTIALS}"]) {
-            sh "ssh -t -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker rm -f ${CONTAINER_NAME}"            
+            sshagent(['PASSWORD']) {
+            sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker rm -f ${CONTAINER_NAME}"            
          }
        }
      }
@@ -36,14 +36,14 @@ def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTA
 ******  Function to Deploye NEW Containers & Start                  *******
 ****************************************************************************/
 
-def DeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String DOCKER_USER, String DOCKER_APP_NAME, String DOCKER_TAG, String CONTAINER_NAME, String LINUX_CREDENTIALS)
+def DeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String DOCKER_USER, String DOCKER_APP_NAME, String DOCKER_TAG, String CONTAINER_NAME)
 {
    try {
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => Docker WebApp Deployment is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
         for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-        sshagent(["${LINUX_CREDENTIALS}"]) {
-        sh "ssh -t -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}"
+        sshagent(['PASSWORD']) {
+        sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}"
          }
        }
      }
