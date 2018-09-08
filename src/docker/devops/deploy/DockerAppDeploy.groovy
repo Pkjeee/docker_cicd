2cd8b5df1f398b91c3dac33d14a07e85ac22aec5
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-08 20:44:53 +0530

    Pseudo-terminal will not be allocated because stdin is not a terminal.

--- src/docker/devops/deploy/DockerAppDeploy.groovy
+++ src/docker/devops/deploy/DockerAppDeploy.groovy
@@ -17,11 +17,11 @@ def UnDeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String CONTA
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => UnDeploy Docker Conatiner is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
 	for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-//	   withCredentials([usernameColonPassword(credentialsId: 'LINUX-PASS', variable: 'PASSWORD')]) {
-//            sshagent(['PASSWORD']) {
-            sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} | docker rm -f ${CONTAINER_NAME}"
-//	    }
-//	 }
+	   withCredentials([usernameColonPassword(credentialsId: 'SSH-KEY-102', variable: 'SSH-KEY-102')]) {
+            sshagent(['SSH-KEY-102']) {
+            sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker rm -f ${CONTAINER_NAME}"
+	    }
+	 }
        }
      }
    }
@@ -44,10 +44,11 @@ def DeployContainer(String DEPLOYMENT_SERVERS, String LINUX_USER, String DOCKER_
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32mINFO => Docker WebApp Deployment is in progress at ${DEPLOYMENT_SERVERS}, please wait..."
         for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
-//	withCredentials([usernameColonPassword(credentialsId: 'LINUX-PASS', variable: 'PASSWORD')]) {
-//        sshagent(['PASSWORD']) {
-        sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} | docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}"
-//	  }
+	withCredentials([usernameColonPassword(credentialsId: 'SSH-KEY-102', variable: 'SSH-KEY-102')]) {
+        sshagent(['SSH-KEY-102']) {
+        sh "ssh -o StrictHostKeyChecking=no ${LINUX_USER}@${LINUX_SERVER} && docker run -p 8080:8080 -d --name ${CONTAINER_NAME} ${DOCKER_USER}/${DOCKER_APP_NAME}:${DOCKER_TAG}" 
+	   }
+	 }
        }
      }
    }
